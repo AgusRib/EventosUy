32cbbcf8f49043998b03116773a06f17bc3cf0b1
Franco Galasso <francof16bolso@gmail.com>
2025-08-23 16:23:42 -0300

    emprolijamiento de frmModificarDatosUsuario

--- gui_entrega2/src/front/ModificarDatosUsuario.java
+++ gui_entrega2/src/front/ModificarDatosUsuario.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.Box;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Font;
 
 public class ModificarDatosUsuario extends JInternalFrame {
 
@@ -29,59 +30,61 @@ public class ModificarDatosUsuario extends JInternalFrame {
 		horizontalBox.setBounds(0, 0, 1, 1);
 		getContentPane().add(horizontalBox);
 		
-		JLabel lblUsuario = new JLabel("Usuario: ");
-		lblUsuario.setBounds(12, 12, 76, 17);
+		JLabel lblUsuario = new JLabel("Seleccionar Usuario:");
+		lblUsuario.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblUsuario.setBounds(58, 16, 156, 17);
 		getContentPane().add(lblUsuario);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(90, 7, 124, 26);
+		comboBox.setBounds(214, 12, 124, 26);
 		getContentPane().add(comboBox);
 		
+		
 		JLabel lblNombre = new JLabel("Nombre: ");
-		lblNombre.setBounds(12, 77, 60, 17);
+		lblNombre.setBounds(107, 69, 60, 17);
 		getContentPane().add(lblNombre);
 		
 		JLabel lblNombre_1 = new JLabel("nickname:");
-		lblNombre_1.setBounds(12, 101, 68, 17);
+		lblNombre_1.setBounds(107, 93, 68, 17);
 		getContentPane().add(lblNombre_1);
 		
 		JLabel lblNombre_2 = new JLabel("Email:");
-		lblNombre_2.setBounds(12, 130, 60, 17);
+		lblNombre_2.setBounds(107, 122, 60, 17);
 		getContentPane().add(lblNombre_2);
 		
 		JLabel lblFechaDeNacimiento = new JLabel("Fecha de nacimiento:");
-		lblFechaDeNacimiento.setBounds(12, 159, 133, 17);
+		lblFechaDeNacimiento.setBounds(107, 155, 133, 17);
 		getContentPane().add(lblFechaDeNacimiento);
 		
 		JLabel lblApellido = new JLabel("Apellido:");
-		lblApellido.setBounds(12, 191, 60, 17);
+		lblApellido.setBounds(107, 173, 60, 17);
 		getContentPane().add(lblApellido);
 		
 		txtFieldNombreUsuario = new JTextField();
-		txtFieldNombreUsuario.setBounds(78, 77, 114, 17);
+		txtFieldNombreUsuario.setBounds(247, 69, 114, 17);
 		getContentPane().add(txtFieldNombreUsuario);
 		txtFieldNombreUsuario.setColumns(10);
 		
 		txtFieldNicknameUsuario = new JTextField();
 		txtFieldNicknameUsuario.setEditable(false);
 		txtFieldNicknameUsuario.setColumns(10);
-		txtFieldNicknameUsuario.setBounds(90, 101, 114, 17);
+		txtFieldNicknameUsuario.setBounds(247, 93, 114, 17);
 		getContentPane().add(txtFieldNicknameUsuario);
 		
 		txtFieldEmailUsuario = new JTextField();
 		txtFieldEmailUsuario.setEditable(false);
 		txtFieldEmailUsuario.setColumns(10);
-		txtFieldEmailUsuario.setBounds(60, 130, 114, 17);
+		txtFieldEmailUsuario.setBounds(247, 122, 114, 17);
 		getContentPane().add(txtFieldEmailUsuario);
 		
 		txtFieldfNacUsuario = new JTextField();
 		txtFieldfNacUsuario.setColumns(10);
-		txtFieldfNacUsuario.setBounds(152, 157, 114, 17);
+		txtFieldfNacUsuario.setBounds(247, 153, 114, 17);
 		getContentPane().add(txtFieldfNacUsuario);
 		
 		txtFieldApellidoUsuario = new JTextField();
 		txtFieldApellidoUsuario.setColumns(10);
-		txtFieldApellidoUsuario.setBounds(78, 189, 114, 17);
+		txtFieldApellidoUsuario.setBounds(247, 173, 114, 17);
 		getContentPane().add(txtFieldApellidoUsuario);
 		
 		JButton btnConfirmarEdicionUsuario = new JButton("Confirmar");
