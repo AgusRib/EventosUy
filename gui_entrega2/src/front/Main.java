ad26e50fcabac720a797aa738b9950eb1f6cb4a4
Franco Galasso <francof16bolso@gmail.com>
2025-08-23 15:42:26 -0300

    se agrega toFront a los frames de Alta y de Modificacion de usuario y se corrije tamanio del frame de modificar datos

--- gui_entrega2/src/front/Main.java
+++ gui_entrega2/src/front/Main.java
@@ -12,6 +12,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Main {
 
@@ -37,9 +39,6 @@ public class Main {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Main() {
 		frmMain = new JFrame();
 		frmMain.setTitle("Main");
@@ -82,6 +81,7 @@ public class Main {
 		mntmModificarDatos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frmModificarDatosUsuario.setVisible(true);
+				frmModificarDatosUsuario.toFront();
 			}
 		});
 		
