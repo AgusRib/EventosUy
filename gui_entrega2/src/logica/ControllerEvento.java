aab27be4e4567ecfd071daf4db909616c815c777
agusrib <agusrrib555@gmail.com>
2025-08-27 12:36:10 -0300

    Controller evento ahora si

---
+++ gui_entrega2/src/logica/ControllerEvento.java
@@ -0,0 +1,59 @@
+package logica;
+import java.util.Set;
+
+public class ControllerEvento implements IControllerEvento{
+	
+	
+	@Override
+	public Set<String> listarEventos() {
+		manejadorEvento mE = manejadorEvento.getInstance();
+		eventos = mE.obtenerEventos();
+		for 
+		return null;
+	}
+
+	@Override
+	public Set<String> listarEdiciones(String nombreEvento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> listarPatrocinios(String nombreEdi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DTPatrocinio obtenerPatrocinio(String nombreEdi, String nombreInstitucion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DTEdicion mostrarDetallesEdicion(String nombreEdi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DTTRegistro verDetalleTRegistro(String nombreEdi, String nomTRegistro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void altaTipoDeRegistro(String nombreEdi, String nombre, Float costo, int cupo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public DTDetalleEvento verDetalleEvento(String nombreEvento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
