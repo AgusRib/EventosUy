5f25a6ab2a2f7be8011e1c9eca2c26f63d1b8b2a
Nacho Tejera <ignaciotema@gmail.com>
2025-08-31 18:35:15 -0300

    Actualizar pruebas en TestControllerUsuario y mejorar la gestión de excepciones en ControllerUsuario

--- gui_entrega2/src/casosPrueba/TestControllerUsuario.java
+++ gui_entrega2/src/casosPrueba/TestControllerUsuario.java
@@ -17,9 +17,8 @@ import logica.Organizador;
 public class TestControllerUsuario {
 		
 	@Test
-	public void testAltaUsuarioInstitucion() {
-		
-		
+	public void testAltaUsuarioInstitucion() throws Exception {
+
 		IControllerUsuario ICU = Factory.getInstance().getControllerUsuario();
 		ManejadorUsuario mU = ManejadorUsuario.getInstance();
 		
@@ -55,7 +54,7 @@ public class TestControllerUsuario {
 			e.printStackTrace();
 		}
 		
-		HashSet institucionesSet = new HashSet<String>();
+		HashSet<String> institucionesSet = new HashSet<String>();
 		institucionesSet.add("Instituto Tecnologico");
 		institucionesSet.add("Instituto de Artes");
 		assertEquals(true, ICU.listarInstituciones().equals(institucionesSet));
@@ -90,10 +89,50 @@ public class TestControllerUsuario {
 			e.printStackTrace();
 		}
 		
+		try {
+			ICU.ingresarAsistente("jorge", "Jorge", "jorge@gmail.com", "Gonzalez", LocalDate.of(2005, 05, 05));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		HashSet<String> usuariosSet = new HashSet<String>();
 		usuariosSet.add("ignaciotema");
 		usuariosSet.add("nachito");
+		usuariosSet.add("jorge");
 		assertEquals(true, ICU.listarUsuarios().equals(usuariosSet));
+		
+		HashSet<String> asistentesSet = new HashSet<String>();
+		asistentesSet.add("ignaciotema");
+		asistentesSet.add("jorge");
+		assertEquals(true, ICU.listarAsistentes().equals(asistentesSet));
+		
+		
+		
+		
+		
+		CargaDatos.cargarDatos();
+
+		// TEST LISTAR REGISTROS A EDICIONES
+		HashSet<String> registros = new HashSet<String>();
+		registros.add("Maratón de Montevideo 2025");
+		registros.add("Maratón de Montevideo 2024");
+		registros.add("Montevideo Rock 2025");
+		
+		assertEquals(true, ICU.listarRegistrosAEventos("sofirod").equals(registros));
+		
+		
+		
+		
+		//TEST LISTAR EDICIONES ORGANIZADAS
+		HashSet<String> ediciones = new HashSet<String>();
+		ediciones.add("Maratón de Montevideo 2025");
+		ediciones.add("Maratón de Montevideo 2024");
+		ediciones.add("Montevideo Rock 2025");
+		ediciones.add("Maratón de Montevideo 2022");
+		
+		assertEquals(true, ICU.listarEdicionesOrganizadas("imm").equals(ediciones));
 		
 	}
 	
