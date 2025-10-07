6c2698516d7bc8f7de0cd2ad01680f0bc61ffbfa
Agustín Marcio Ribeiro García <agustin.ribeiro@fing.edu.uy>
2025-10-07 19:05:20 -0300

    Replace ControllerEvento.java

--- gui_entrega2/src/logica/models/ControllerEvento.java
+++ gui_entrega2/src/logica/models/ControllerEvento.java
@@ -1,4 +1,4 @@
-package logica.models;
+package logica;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,19 +12,7 @@ import excepciones.FechaInicioPREALTA;
 import excepciones.FechaRegPREALTA;
 import excepciones.NombreEdicionExistenteExcepcion;
 import excepciones.NombreEventoExcepcion;
-import logica.controllers.IControllerEvento;
-import logica.dataTypes.DTAsistente;
-import logica.dataTypes.DTDetalleEdicion;
-import logica.dataTypes.DTDetalleEvento;
-import logica.dataTypes.DTPatrocinio;
-import logica.dataTypes.DTRegistro;
-import logica.dataTypes.DTTipoRegistro;
-import logica.enumerators.EstadoEdicion;
-import logica.enumerators.NivelPatrocinio;
-import logica.manejadores.ManejadorCategoria;
-import logica.manejadores.ManejadorEdicion;
-import logica.manejadores.ManejadorEvento;
-import logica.manejadores.ManejadorUsuario;
+
 
 public class ControllerEvento implements IControllerEvento{
 	
@@ -70,9 +58,7 @@ public class ControllerEvento implements IControllerEvento{
 		Set<String> ediciones = new LinkedHashSet<>();
 		
 		if(ev != null) {
-			for (Edicion ed : ev.getColEdiciones()) {
-				ediciones.add( ed.getNombre() );
-			}
+		  ediciones = ev.getEdiciones();
 		}
 		return ediciones;
 	}
@@ -182,7 +168,7 @@ public class ControllerEvento implements IControllerEvento{
 
 		Edicion nueva = new Edicion(nombre, sigla, fechaInicio, fechaFin, fechaAlta, ciudad, pais,ev,org);
 		ev.agregarEdicion(nueva); 
-		mEdi.agregarEdicion(nueva);
+		mEdi.agregarEdicionIngresada(nueva);
 		
 	}
 	
@@ -191,7 +177,7 @@ public class ControllerEvento implements IControllerEvento{
 		Edicion edi = mEdi.encontrarEdicion(nombreEdi);
 		if(aceptar && edi.getEstado() == EstadoEdicion.Ingresada) {
 			edi.setEstado(EstadoEdicion.Confirmada);
-			mEdi.agregarEdicion(edi);
+			mEdi.agregarEdicionIngresada(edi);
 		} else {
 			edi.setEstado(EstadoEdicion.Rechazada);
 			//evaluar si hay que eliminar de alguna coleccion
@@ -208,7 +194,7 @@ public class ControllerEvento implements IControllerEvento{
 	public Set<DTAsistente> listarAsistentesAEdicionDeEvento(String nomEdi) {
 		ManejadorEdicion mEdi = ManejadorEdicion.getInstance();
 		Edicion edi = mEdi.encontrarEdicion(nomEdi);
-		return edi.obtenerAsistentes();
+		return edi.obtenerAsistentes()	;
 	}
 	
 	@Override
@@ -281,6 +267,29 @@ public class ControllerEvento implements IControllerEvento{
 		return ev.getNombre();
 	}
 	
+	public void AceptarEdicion(String nomedi, String nomev) {
+		ManejadorEvento mE = ManejadorEvento.getInstance();
+		Evento ev = mE.obtenerEvento(nomev);
+		Edicion edi = ev.getEdicion(nomedi);
+		ev.CambioEstado(edi,EstadoEdicion.Confirmada);
+		edi.setEstado(EstadoEdicion.Confirmada);
+		ManejadorEdicion mEdi = ManejadorEdicion.getInstance();
+		mEdi.CambioEstado(edi,EstadoEdicion.Confirmada);
+		
+		
+	}
+	
+	public void RechazarEdicion(String nomedi, String nomev) {
+		ManejadorEvento mE = ManejadorEvento.getInstance();
+		Evento ev = mE.obtenerEvento(nomev);
+		Edicion edi = ev.getEdicion(nomedi);
+		ev.CambioEstado(edi,EstadoEdicion.Rechazada);
+		edi.setEstado(EstadoEdicion.Rechazada);
+		ManejadorEdicion mEdi = ManejadorEdicion.getInstance();
+		mEdi.CambioEstado(edi,EstadoEdicion.Rechazada);
+		
+		
+	}
 	
 	
 }
