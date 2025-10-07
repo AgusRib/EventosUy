2b25ace11eac9c5004c5fb18a2356726f16c11b1
Agustín Marcio Ribeiro García <agustin.ribeiro@fing.edu.uy>
2025-10-07 19:08:33 -0300

    Replace ManejadorEdicion.java

--- gui_entrega2/src/logica/manejadores/ManejadorEdicion.java
+++ gui_entrega2/src/logica/manejadores/ManejadorEdicion.java
@@ -1,15 +1,17 @@
-package logica.manejadores;
+package logica;
 
 import java.util.HashMap;
 
-import logica.models.Edicion;
-
 public class ManejadorEdicion {
 	private static ManejadorEdicion instance;
-	private HashMap<String, Edicion> colEdiciones;
+	private HashMap<String, Edicion> colEdicionesPendientes;
+	private HashMap<String, Edicion> colEdicionesConfirmadas;
+	private HashMap<String, Edicion> colEdicionesRechazadas;
 
 	private ManejadorEdicion() {
-		colEdiciones = new HashMap<String, Edicion>();
+		colEdicionesPendientes = new HashMap<String, Edicion>();
+		colEdicionesConfirmadas = new HashMap<String, Edicion>();
+		colEdicionesRechazadas = new HashMap<String, Edicion>();
 	}
 	
 	public static ManejadorEdicion getInstance() {
@@ -18,19 +20,49 @@ public class ManejadorEdicion {
 		}
 		return instance;
 	}
-	
-	public boolean existeEdicion(String nickname) {
-		return colEdiciones.containsKey(nickname);
-	}
-	
-	public HashMap<String,Edicion> obtenerEdiciones() {
-		return colEdiciones;
-	}
-	public Edicion encontrarEdicion(String nombreEdi) {
-		return (Edicion) colEdiciones.get(nombreEdi);
+
+	// Verifica si existe una edición en alguna de las colecciones
+	public boolean existeEdicion(String nombreEdicion) {
+		return colEdicionesPendientes.containsKey(nombreEdicion)
+			|| colEdicionesConfirmadas.containsKey(nombreEdicion)
+			|| colEdicionesRechazadas.containsKey(nombreEdicion);
 	}
 
-	public void agregarEdicion(Edicion nueva) {
-		colEdiciones.put(nueva.getNombre(), nueva);
+	// Obtener todas las ediciones Pendientes
+	public HashMap<String,Edicion> obtenerEdicionesPendientes() {
+		return colEdicionesPendientes;
+	}
+
+	// Obtener todas las ediciones confirmadas
+	public HashMap<String,Edicion> obtenerEdicionesConfirmadas() {
+		return colEdicionesConfirmadas;
+	}
+
+	// Obtener todas las ediciones rechazadas
+	public HashMap<String,Edicion> obtenerEdicionesRechazadas() {
+		return colEdicionesRechazadas;
+	}
+
+	// Buscar una edición por nombre en todas las colecciones
+	public Edicion encontrarEdicion(String nombreEdi) {
+		if (colEdicionesPendientes.containsKey(nombreEdi)) {
+			return colEdicionesPendientes.get(nombreEdi);
+		} else if (colEdicionesConfirmadas.containsKey(nombreEdi)) {
+			return colEdicionesConfirmadas.get(nombreEdi);
+		} else if (colEdicionesRechazadas.containsKey(nombreEdi)) {
+			return colEdicionesRechazadas.get(nombreEdi);
+		}
+		return null;
+	}
+
+	// Agregar edición a Pendientes
+	public void agregarEdicionIngresada(Edicion nueva) {
+		colEdicionesPendientes.put(nueva.getNombre(), nueva);
+	}
+
+	public void CambioEstado(Edicion edi,EstadoEdicion  nuevoestado) {
+		colEdicionesPendientes.remove(edi.getNombre());
+		colEdicionesConfirmadas.put(edi.getNombre(), edi);
+		
 	}
 }
