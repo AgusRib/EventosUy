ec096dab7f93153b055b42f0f51df140030d5818
Franco Galasso <francof16bolso@gmail.com>
2025-10-08 13:27:22 -0300

    se crea carpeta templates y ServletUsuario

--- tarea2/src/main/java/ServletUsuario.java
+++ tarea2/src/main/java/ServletUsuario.java
@@ -6,11 +6,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import logica.IControllerUsuario;
-import logica.ControllerUsuario;
-import logica.Usuario;
 import logica.ManejadorUsuario;
+import logica.models.Usuario;
 
 
 /**
