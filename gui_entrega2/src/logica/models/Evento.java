b0ea3b3ff9cf4f8af1d98253a88cd655eaa7c11b
ignacio.tejera <ignacio.tejera@fing.edu.uy>
2025-11-08 19:53:44 -0300

    agrega soporte para JPA y configuraciones de persistencia

--- gui_entrega2/src/logica/models/Evento.java
+++ gui_entrega2/src/logica/models/Evento.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import logica.data_types.DTDetalleEvento;
 import logica.enumerators.EstadoEdicion;
+import jakarta.persistence.Entity;
 
 public class Evento{
 	private String nombre;
