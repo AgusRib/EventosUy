bfb3779c7c36bf539fc64f73463ee514d16429a6
ignacio.tejera <ignacio.tejera@fing.edu.uy>
2025-11-08 21:18:25 -0300

    agrega método archivarEdicion en IControllerEvento

--- gui_entrega2/src/logica/controllers/IControllerEvento.java
+++ gui_entrega2/src/logica/controllers/IControllerEvento.java
@@ -53,6 +53,7 @@ public interface IControllerEvento {
 	Set<String> listarEdicionesPendientes(String nombreEvento);
 	public void finalizarEvento(String nombreEvento);
     void confirmarAsistencia(String nombreEdi, String nickAsistente);
+    void archivarEdicion(String nombreEdi);
 	
 	
 }
