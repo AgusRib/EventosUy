8e02352b7f339e6fabe30e139cd2f40549edb984
agusrib <agusrrib555@gmail.com>
2025-11-10 09:52:47 -0300

    cargadatos actualizado

--- gui_entrega2/src/casosPrueba/CargaDatos.java
+++ gui_entrega2/src/casosPrueba/CargaDatos.java
@@ -38,6 +38,7 @@ public class CargaDatos {
 		cargarTiposRegistro();
 		cargarPatrocinios();
 		cargarRegistros();
+		cargarSeguidores();
 		
 		System.out.println("Carga de datos finalizada");
 	    imprimirDatosCargados();
@@ -172,7 +173,20 @@ public class CargaDatos {
 			}
 			
 			ICE.altaEvento(nombre, sigla, LocalDate.parse(fechaAlta), descripcion, categorias,"");
+			
+			// Manejar el estado finalizado (campos[7])
+			String finalizado = campos[7];
+			if (finalizado.equals("Si")) {
+				// Acceder al manejador para establecer el estado finalizado del evento
+				ManejadorEvento mE = ManejadorEvento.getInstance();
+				Evento evento = mE.obtenerEvento(nombre);
+				if (evento != null) {
+					evento.setFinalizado(true);
+				}
+			}
 		}
+		System.out.println("Eventos cargados");
+		brEventos.close();
 	}
 	
 	
@@ -205,6 +219,8 @@ public class CargaDatos {
 				String fechaIni = campos[7];
 				String fechaFin = campos[8];
 				String fechaAlta = campos[9];
+				// campos[11] es Imagen - lo ignoramos
+				String videoUrl = campos.length > 12 ? campos[12] : ""; // Video URL
 				
 				String nombreEvento = buscarLinea(idEve, "/datosPrueba/2025Eventos.csv")[1];
 				String nicknameOrganizador = buscarLinea(idOrg, "/datosPrueba/2025Usuarios.csv")[2];
@@ -218,7 +234,7 @@ public class CargaDatos {
 				String[] fechaAltaParts = fechaAlta.split("/");
 				fechaAlta = new String(fechaAltaParts[2] + "-" + fechaAltaParts[1] + "-" + fechaAltaParts[0]);
 				
-				ICE.altaEdicionDeEvento(nombreEvento, nicknameOrganizador, nombre, sigla, LocalDate.parse(fechaIni), LocalDate.parse(fechaFin), LocalDate.parse(fechaAlta), ciudad, pais,"");
+				ICE.altaEdicionDeEvento(nombreEvento, nicknameOrganizador, nombre, sigla, LocalDate.parse(fechaIni), LocalDate.parse(fechaFin), LocalDate.parse(fechaAlta), ciudad, pais, videoUrl);
 			
 				if (campos[10].equals("Aceptada")) {
 					ICE.aceptarEdicion(nombre,nombreEvento);
@@ -247,7 +263,7 @@ public class CargaDatos {
 			}
 		
 	
-		System.out.println("Ediciones cargadas");};
+		System.out.println("Ediciones cargadas");}
 			
 	
 		
@@ -409,14 +425,47 @@ public class CargaDatos {
 	
 		System.out.println("Registros cargados");
 	}
+	
+	private static void cargarSeguidores() {
+		BufferedReader brSeguidores;
+		try {
+			brSeguidores = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/datosPrueba/2025SeguidoresSeguidos.csv"));
 
+			IControllerUsuario ICU = Factory.getInstance().getControllerUsuario();
+			
+			String linea;
+			brSeguidores.readLine(); // Saltear la primer linea (headers)
+			while ((linea = brSeguidores.readLine()) != null) {
+				if (linea.isBlank())
+					continue;
+				
+				String[] campos = linea.split(";");
+							
+				String ref = campos[0];
+				String refSeguidor = campos[1];
+				String nicknameSeguidor = campos[2];
+				String refSeguido = campos[3];
+				String nicknameSeguido = campos[4];
+				
+				ICU.seguirUsuario(nicknameSeguidor, nicknameSeguido);
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		System.out.println("Seguidores cargados");
+	}
 	
 	
 	
-	
-	
-	
-		
 	//UTILS
 	private static String[] buscarLinea(String id, String path) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + path));
@@ -490,7 +539,7 @@ public class CargaDatos {
 	}
 
 	private static void imprimirCategorias() {
-	    // Si tenés ManejadorCategoria con Set<String> o Map<String,Categoria>
+	    // Si tenés ManejadorCategoria with Set<String> o Map<String,Categoria>
 	    ManejadorCategoria mC = ManejadorCategoria.getInstance();
 	    java.util.Set<String> nombres = mC.obtenernombresCategorias(); // o adaptá según tu API
 	    System.out.println("\n[Categorias] total = " + (nombres == null ? 0 : nombres.size()));
